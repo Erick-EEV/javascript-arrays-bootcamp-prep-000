1fbc320fa513d584bdd758f029ee8f10a8d61e67
Erick Villeda <eeverickeev@gmail.com>
2020-07-25 22:46:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,6 +22,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array;
 }
 function removeElementFromBeginningOfArray(array){
-  array.slice(1);
+  array.slice(0);
   return array;
 }
