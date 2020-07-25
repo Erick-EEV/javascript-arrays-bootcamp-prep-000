6c7f7eb074ec52aa6b07b43bbe54c633c4d49b80
Erick Villeda <eeverickeev@gmail.com>
2020-07-25 22:20:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,5 +15,5 @@ function destructivelyAddElementToEndOfArray(array, element){
   return array;
 }
 function accessElementInArray(array, index, element){
-  return (element)
+  return element(index)
 }
