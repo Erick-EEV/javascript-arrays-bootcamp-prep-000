3084347987f3d7070ae24f9b62e5aeea3d174b7f
Erick Villeda <eeverickeev@gmail.com>
2020-07-25 23:10:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,5 @@ function removeElementFromBeginningOfArray(array){
 return array.slice(-2);
 }
 function destructivelyRemoveElementFromEndOfArray(array){
-return array.push(-2);  
+return array.push();  
 }
